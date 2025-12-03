0f4a14e77b37ffc49e9e4a1226c33c6ccf431269
Mukunda Hari <mukundharirudra@gmail.com>
2025-12-03 23:06:14 +0530

    env var

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -15,7 +15,7 @@ export const DB = Symbol('DB');
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
         const pool = new Pool({
-          connectionString: 'psql postgresql://neondb_owner:npg_1twSWPBvmb5Z@ep-shy-waterfall-a18rfqbq-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+          connectionString: 'postgresql://neondb_owner:npg_1twSWPBvmb5Z@ep-shy-waterfall-a18rfqbq-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
           ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
         });
         return drizzle(pool, { schema });
