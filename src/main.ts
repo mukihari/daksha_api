06931ff44832c6a22440da12b41207cf0e3da14c
Mukunda Hari <mukundharirudra@gmail.com>
2025-12-03 08:30:51 +0530

    enable cors origin url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://daksha-api.vercel.app', 
+    origin: [
+      'https://daksha-frontend-git-main-mukiharis-projects.vercel.app',
+      'https://daksha-frontend-g7r3t95um-mukiharis-projects.vercel.app',
+      'http://localhost:4200',
+      'http://localhost:3000',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
